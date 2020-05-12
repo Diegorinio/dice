f631a3095c51a0bd3fb44e91bb6928406157d86a
Zbyszggo <zbyslaf321@gmail.com>
2020-05-12 21:30:37 +0200

    Fix random, send via web

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 let diceThrowResult
+let resultsHistory = []
 const audio = new Audio('dice.wav')
 tellMeSomething = (text) => {
     const lang = 'pl-PL'
     const tell = new SpeechSynthesisUtterance(text)
-    tell.pitch = 1.7
+    tell.pitch = 1.2
+    tell.volume =5
     tell.lang = lang
     speechSynthesis.speak(tell)
 }
 diceThrow = () => {
+    document.querySelector('.dice-button').disabled = true
     resetDiceVals()
     audio.play()
-    diceThrowResult = getRandomInt(6);
-    showResultOnDice()
-    tellMeSomething(`Wynik to ${getDiceResult()}`)
+    const interval = setInterval(diceAnimation,100)
+    setTimeout(()=>{
+        diceThrowResult = getRandomInt(6);
+        showResultOnDice()
+        tellMeSomething(`Wynik rzutu to  ${getDiceResult()}`)
+        document.querySelector('.dice-button').disabled = false
+        document.querySelector('.act-result').innerHTML = diceThrowResult
+        document.querySelector('.results-history').innerHTML += `${diceThrowResult} `
+        diceThrowResult = ''
+    },parseInt(audio.duration)*1000)
+    setTimeout(() => {
+        clearInterval(interval)    
+    }, 3800);
 }
+
 getRandomInt = (max) => {
     return Math.floor(Math.random() * (max + 1 - 1) + 1);
 }
@@ -21,6 +35,7 @@ getDiceResult = () => {
     return diceThrowResult
 }
 showResultOnDice = () => {
+    resetDiceVals()
     const res = parseInt(getDiceResult())
     const dice = [
         0, 0, 0,
@@ -75,7 +90,8 @@ resetDiceVals = () => {
     });
 }
 diceAnimation = () => {
-
+    resetDiceVals()
+    diceThrowResult = getRandomInt(6)
+    console.log(diceThrowResult)
+    showResultOnDice()
 }
-
-// tellMeSomething('Ty kurwa fbi')
